44b4600c1a28f77c0be816d07373914dd7e67d4b
Qasim Subhani <Qasim.subhani@cyclonesmartsolutions.com>
2021-06-25 22:57:04 +0500

    After Setting Git

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
